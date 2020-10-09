582b78b8e21da4ae1295a173eb6083a969c5ae24
Sandra <sandrabess@hotmail.com>
2020-10-09 12:11:52 -0400

    ex 8 completed

--- __workshop/exercise-files/script.js
+++ __workshop/exercise-files/script.js
@@ -1,4 +1,23 @@
 // add js here
+const clearErrors = (error, password, confirmPassword) => {
+    error.classList.remove("error-display");   
+    password.classList.remove("input-error");
+    confirmPassword.classList.remove("input-error");    
+ }
+ 
+const createPassword = () => {
+    const wordArray = ["apple", "banana", "table", "decision", "art", "beautiful", "light", "meditation",
+                    "yoga", "zen", "color", "election", "fraud", "healing", "eat", "cloud", "sun", "illumination", 
+                    "opening", "cinema", "chair", "movie", "theater", "space"];
+
+    let passString = "";
+    while(passString.length <= 25) {
+        const randomIndex = Math.floor(Math.random() * wordArray.length);  
+        passString += wordArray[randomIndex] + "-";
+    }
+    return passString.slice(0, passString.length -1);
+}
+
 function submitForm(event) {
   
     let isError = false;
@@ -14,8 +33,9 @@ function submitForm(event) {
         isError = true;        
     }    
     else if (password.value.length < 10) {        
-        error.classList.add("error-display");        
-        errorText.innerText = "Your password is too short! Please provide a password that is at least 10 characters long.";
+        error.classList.add("error-display"); 
+        const newPassword = createPassword();       
+        errorText.innerText = "Your password is too short! How about \"" + newPassword + "\"?.";
         password.classList.add("input-error");
         password.focus();
         isError = true;
@@ -35,18 +55,18 @@ function submitForm(event) {
     
     event.preventDefault();
   }
-
- const clearErrors = (error, password, confirmPassword) => {
-    error.classList.remove("error-display");   
-    password.classList.remove("input-error");
-    confirmPassword.classList.remove("input-error");    
- }
+ 
 
 const form = document.querySelector('form');
 form.addEventListener('submit', submitForm);
 
 const clearBtn = document.querySelector('.clear-btn');
 clearBtn.addEventListener("click", function(event) {
+    const password = document.getElementById("password");
+    const confirmPassword = document.getElementById("cpassword");
+    const error = document.querySelector('.error');
+    clearErrors(error, password, confirmPassword);
     form.reset();
     event.preventDefault();
 });
+
